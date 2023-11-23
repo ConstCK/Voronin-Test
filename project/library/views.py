424710da22efbbcf1009fa40cf7e0c6331342b1b
ConstCK <Constantin_CK@outlook.com>
2023-11-23 20:16:05 +0300

    Add viewset for books data fetching and posting

--- project/library/views.py
+++ project/library/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
