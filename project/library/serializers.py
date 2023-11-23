6fd3a038be25845b988851b84cc32fbfae755dfd
ConstCK <Constantin_CK@outlook.com>
2023-11-23 20:07:30 +0300

    Add serializers

--- project/library/serializers.py
+++ project/library/serializers.py
@@ -1,1 +1,16 @@
 from rest_framework import serializers
+
+from .models import Book, Author
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Book
+        depth = 1
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ['name']
+        model = Author
